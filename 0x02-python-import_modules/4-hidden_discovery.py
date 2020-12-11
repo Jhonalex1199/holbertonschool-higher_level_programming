74697dfb1f72a4fc8c2e4a53baedacaffdcfd2e3
Jhonalex1199 <2050@holbertonschool.com>
2020-12-10 19:58:43 -0500

    py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,5 @@ import hidden_4
 if __name__ == '__main__':
     x = dir(hidden_4)
     for i in range(x):
-        if x[i][0] != '_':
-            print(x[i])
+        if x[i] != '_':
+            print(x)
