e215886622545172f1251deed6cbb4632faaa992
jhonalex1199 <2050@holbertonschool.com>
2020-12-15 15:03:33 -0500

    c

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    xlist = []
-    xlist = my_list.copy()
-    xlist.reverse()
-    i = 0
-    while (i < len(xlist)):
-        print("{:d}".format(xlist[i]))
-        i += 1
+    if (my_list):
+        xlist = []
+        xlist = my_list.copy()
+        xlist.reverse()
+        i = 0
+        while (i < len(xlist)):
+            print("{:d}".format(xlist[i]))
+            i += 1
