112d79609aa97811e78f39733eb5d3e24992e366
jhonalex1199 <2050@holbertonschool.com>
2020-12-15 16:25:27 -0500

    py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     if (my_list):
         x = len(my_list)
         my_list2 = my_list.copy()
-        if (idx < 0 or idx > x):
+        if (idx < 0 or idx >= x):
             return (my_list2)
         i = 0
         while (i <= idx):
