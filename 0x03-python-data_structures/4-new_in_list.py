91defadc3e687d2a0729fef95782c3da4a2b8648
jhonalex1199 <2050@holbertonschool.com>
2020-12-15 16:21:57 -0500

    c

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if (my_list):
+        x = len(my_list)
+        my_list2 = my_list.copy()
+        if (idx < 0 or idx > x):
+            return (my_list2)
+        i = 0
+        while (i <= idx):
+            if (i == idx):
+                my_list2[idx] = element
+            i += 1
+        return (my_list2)
