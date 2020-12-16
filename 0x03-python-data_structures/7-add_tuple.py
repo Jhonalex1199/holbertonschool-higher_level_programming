17739fc92b792cd75f02d0c50d3407f77705f480
jhonalex1199 <2050@holbertonschool.com>
2020-12-15 19:19:04 -0500

    py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,11 +2,14 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     list1 = [0, 0]
     list2 = [0, 0]
+
     lena = len(tuple_a)
     lenb = len(tuple_b)
+
     if lena > 2 and lenb > 2:
         lena = 2
         lenb = 2
+
     for i in range(lena):
         list1[i] += tuple_a[i]
     for j in range(lenb):
