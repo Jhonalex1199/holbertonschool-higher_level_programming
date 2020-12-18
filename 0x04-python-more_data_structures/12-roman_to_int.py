287eba9271d7f34d2ebb862cc77f88bd6593aac8
jhonalex1199 <2050@holbertonschool.com>
2020-12-17 23:36:58 -0500

    py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman):
-    num_romanos = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num_r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = 0
 
     if roman is None or type(roman) != str:
         return 0
 
     for x in range(len(roman)):
-        if x > 0 and num_romanos[roman[x]] > num_romanos[roman[x - 1]]:
-            res += num_romanos[roman[x]] - 2 * num_romanos[roman[x - 1]]
+        if x > 0 and num_r[roman[x]] > num_r[roman[x - 1]]:
+            res += num_r[roman[x]] - 2 * num_r[roman[x - 1]]
         else:
-            res += num_romanos[roman[x]]
+            res += num_r[roman[x]]
     return res
