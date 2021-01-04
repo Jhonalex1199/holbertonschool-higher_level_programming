43b66016773f908d5d403ab1e6b6ba342b2c2cef
jhonalex1199 <2050@holbertonschool.com>
2021-01-04 17:23:48 -0500

    py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end="")
-        print()
-        return x
-    except IndexError:
-        return "No puede ser mayor al tamaño de la lista"
+        except IndexError:
+            break
+    print()
+    return x
