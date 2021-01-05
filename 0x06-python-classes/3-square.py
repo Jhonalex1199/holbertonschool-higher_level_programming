421d35563560b881261049899c2395a66bdf1e62
jhonalex1199 <2050@holbertonschool.com>
2021-01-05 18:11:24 -0500

    py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a documentation of my modules"""
+
+
+class Square:
+    """a documentation of my class"""
+
+    def __init__(self, size=0):
+
+        """ private attribute : .__size """
+
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+
+        """function that return the curret square area"""
+
+        return self.__size ** 2
