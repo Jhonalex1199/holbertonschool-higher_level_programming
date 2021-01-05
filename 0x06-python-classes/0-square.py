38a2009a306a2397c26574744189efe4bd65788e
jhonalex1199 <2050@holbertonschool.com>
2021-01-05 16:42:33 -0500

    py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a documentation of my modules"""
+
+
+class Square:
+
+    """
+    a documentation of my class :
+        creat an empty class name Square
+    """
+
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a documentation of my modules"""
+
+
+class Square:
+
+    """
+    a documentation of my class :
+        create a private intance attribute size
+    """
+
+    def __init__(self, size):
+
+        """ private attribute : .__size """
+
+        self.__size = size
