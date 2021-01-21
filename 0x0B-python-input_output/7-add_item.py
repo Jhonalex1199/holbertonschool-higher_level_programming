43c4de7dbca639cb1419376321fd2d698c4faf71
jhonalex1199 <2050@holbertonschool.com>
2021-01-20 23:38:28 -0500

    py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""module"""
-from sys import argv
-save = __import__("5-save_to_json_file").save_to_json_file
-load = __import__("6-load_from_json_file").load_from_json_file
+"""Load, add, save"""
+import os.path
+import sys
+
+arg = sys.argv[1:]
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-try:
-    txt = load("add_item.json")
-except Exception:
-    txt = []
+my_list = []
 
-txt += argv[1:]
+if os.path.exists("./add_item.json"):
+    my_list = load_from_json_file("add_item.json")
 
-save(txt, "add_item.json")
+save_to_json_file(my_list + arg, "add_item.json")
