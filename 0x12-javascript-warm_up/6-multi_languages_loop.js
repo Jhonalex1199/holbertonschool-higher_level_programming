bc00ecbd520345f5e4b188889fdd9d2da6fe281e
jhonalex1199 <2050@holbertonschool.com>
2021-03-24 10:08:26 -0500

    change issues

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const ele = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const ele = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const x of ele) {
   console.log(x);
 }
