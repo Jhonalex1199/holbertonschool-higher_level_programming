8960e35bedd845fb2d9f2b77fcab3643f125a087
jhonalex1199 <2050@holbertonschool.com>
2021-05-17 11:16:31 -0500

    star wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+$(function () {
+    $.get('https://swapi-api.hbtn.io/api/films/?format=json', (data, status) => {
+      for (const movie of data.results) {
+        $('#list_movies').append($('<li></li>').text(movie.title));
+      }
+    });
+  });
